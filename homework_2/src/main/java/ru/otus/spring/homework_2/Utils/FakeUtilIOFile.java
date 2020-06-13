0a1d6673d4d2035a8a2648498f0f9ae6143022f6
Lera Zoom <lera.zoom.lz@gmail.com>
2020-06-13 15:15:08 +0300

    Fake for testing TestDaoIml. Added FakeUtilIOFile

---
+++ homework_2/src/main/java/ru/otus/spring/homework_2/Utils/FakeUtilIOFile.java
@@ -0,0 +1,31 @@
+package ru.otus.spring.homework_2.Utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeUtilIOFile implements Utils{
+
+    private final String nameFile;
+
+    public FakeUtilIOFile(String inNameFile) {
+        this.nameFile = inNameFile;
+    }
+
+    public List<String> getListStringsFromFile() throws IOException {
+        List<String> listString = new ArrayList<String>();
+        listString.add(0, "Question");
+        listString.add(1, "How much will it be 2 plus 2?");
+        listString.add(2, "Answer");
+        listString.add(3, "4");
+        listString.add(4, "Answer");
+        listString.add(5, "2");
+        listString.add(6, "Answer");
+        listString.add(7, "22");
+        listString.add(8, "TrueAnswer");
+        listString.add(9, "1");
+        listString.add(10, "END");;
+        return listString;
+    }
+
+}
---
+++ homework_2/src/main/java/ru/otus/spring/homework_2/Utils/Utils.java
@@ -0,0 +1,11 @@
+package ru.otus.spring.homework_2.Utils;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Utils {
+
+    public List<String> getListStringsFromFile() throws IOException;
+
+
+}
