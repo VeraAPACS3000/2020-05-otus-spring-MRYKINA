61c7fd191eda1468482e4cccffad56cc3ec7d437
VeraAPACS3000 <LeraZoom@yahoo.com>
2020-07-23 19:26:35 +0300

    Удалила отладочный мусор из main
    Удалила отладочный мусор из main

--- homework_6/src/main/java/ru/otus/spring/homework_6/Main.java
+++ homework_6/src/main/java/ru/otus/spring/homework_6/Main.java
@@ -8,21 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        //SpringApplication.run(Main.class);
-
-        ApplicationContext applicationContext = SpringApplication.run(Main.class);
-
-        /*LibraryService libraryService = applicationContext.getBean(LibraryService.class);
-
-        libraryService.getAllBooks();
-
-        libraryService.deleteBookById(1);
-
-        libraryService.getAllBooks();
-
-        libraryService.getAuthorByName("");
-
-        libraryService.findCommentsByNameBook("To Chaadaev");*/
+        SpringApplication.run(Main.class);
     }
-
 }
