51174a0533f2a88bbe64dc0e4dc6fa1977c9ace6
Lera Zoom <lera.zoom.lz@gmail.com>
2020-07-23 19:47:48 +0300

    Main

--- homework_6/src/main/java/ru/otus/spring/homework_6/Main.java
+++ homework_6/src/main/java/ru/otus/spring/homework_6/Main.java
@@ -2,27 +2,11 @@ package ru.otus.spring.homework_6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        //SpringApplication.run(Main.class);
-
-        ApplicationContext applicationContext = SpringApplication.run(Main.class);
-
-        /*LibraryService libraryService = applicationContext.getBean(LibraryService.class);
-
-        libraryService.getAllBooks();
-
-        libraryService.deleteBookById(1);
-
-        libraryService.getAllBooks();
-
-        libraryService.getAuthorByName("");
-
-        libraryService.findCommentsByNameBook("To Chaadaev");*/
+        SpringApplication.run(Main.class);
     }
-
 }
