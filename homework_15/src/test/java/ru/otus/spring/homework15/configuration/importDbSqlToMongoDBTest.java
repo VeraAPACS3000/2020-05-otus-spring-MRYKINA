ad30d6013bba775a266aec3b0d0002072fb2a397
Lera Zoom <lera.zoom.lz@gmail.com>
2020-11-29 00:22:14 +0300

    потерявшийся @SpringBootTest

--- homework_15/src/test/java/ru/otus/spring/homework15/configuration/importDbSqlToMongoDBTest.java
+++ homework_15/src/test/java/ru/otus/spring/homework15/configuration/importDbSqlToMongoDBTest.java
@@ -7,12 +7,13 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@DataJpaTest
+@SpringBootTest
 @SpringBatchTest
 class importDbSqlToMongoDBTest {
 
