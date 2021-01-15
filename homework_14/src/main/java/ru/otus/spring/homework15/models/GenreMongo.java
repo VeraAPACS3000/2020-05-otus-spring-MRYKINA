25932968a3da90a8b8b2ad288bfcdf76c4c25e33
Lera Zoom <lera.zoom.lz@gmail.com>
2021-01-15 22:50:34 +0300

    batch

---
+++ homework_14/src/main/java/ru/otus/spring/homework15/models/GenreMongo.java
@@ -0,0 +1,50 @@
+package ru.otus.spring.homework15.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "genres")
+public class GenreMongo {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    public GenreMongo() {
+    }
+
+    public GenreMongo(String id, String nameGenre) {
+        this.id = id;
+        this.name = nameGenre;
+    }
+
+    public GenreMongo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "GenreMongo{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
