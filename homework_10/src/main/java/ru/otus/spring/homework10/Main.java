bb4a15bd23a43fd0a47b7519fbf3fa3fb357073b
Lera Zoom <lera.zoom.lz@gmail.com>
2021-01-28 20:22:54 +0300

    Hystrix, eureka, feign

--- homework_10/src/main/java/ru/otus/spring/homework10/Main.java
+++ homework_10/src/main/java/ru/otus/spring/homework10/Main.java
@@ -2,8 +2,10 @@ package ru.otus.spring.homework10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "ru.otus.spring.homework10.feign")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
